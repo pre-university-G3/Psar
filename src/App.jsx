6c36c4a4085089d8275dcd1ab2e84b2d53c3199c
Dana Dorn <dorndana8899@gamil.com>
2025-04-04 16:11:33 +0700

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,18 @@
-<<<<<<< HEAD
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import './App.css';
-import Home from './page/homepage/Home';
-
-=======
 // import { createBrowserRouter, RouterProvider } from "react-router";
 import "./App.css";
 // import RootLayout from "./components/layout/RootLayout";
 import NavbarComponent from "./components/button/navbar/NavbarComponent";
 import FooterComponent from "./components/footer/FooterComponent";
 import ShoppingCart from "./page/shoppingcart/ShoppingCart";
->>>>>>> a2f42645f39eff1fec03d50f9f40ea46e82ddbde
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about",
-      element: <h2>About Us</h2>,
-    }
-    
-     
-  ]);
-
+ 
   return (
-<<<<<<< HEAD
-    <RouterProvider router={router} />
-=======
     <>
-    <NavbarComponent />
+    <NavbarComponent/>
     <ShoppingCart/>
     <FooterComponent/>
     </>
->>>>>>> a2f42645f39eff1fec03d50f9f40ea46e82ddbde
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Aboutus } from "./page/aboutus/Aboutus.jsx";
 import RootLayout from "./components/layout/RootLayout.jsx";
